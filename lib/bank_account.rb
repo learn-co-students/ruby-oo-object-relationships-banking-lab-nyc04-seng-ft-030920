157e7650e5ff679e85e7fd16110503c55cfcbbba
isabelxklee <hello@isabelklee.co>
2020-03-12 17:12:16 -0400

    wrote class methods for bank_account and transfer

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end 
+
+    def deposit(num)
+        @balance += num
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.status == "open" && self.balance > 0
+            true
+        else self.status == "closed"
+            false
+        end 
+    end
+
+    def close_account 
+        self.status = "closed"
+    end 
 
 end
+
+# binding.pry
+# @closed = BankAccount.new("steph")
+# @closed.status = "closed"
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+require 'pry'
+require_relative './bank_account'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  # Transfer.new(amanda, avi, 50) passing
+  # Transfer.new(amanda, avi, 4000) failing
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end 
+
+  def execute_transaction
+    if valid? && @status == "pending" && sender.balance > @amount
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    else
+      reject_transfer
+    end 
+
+  end 
+
+  def reject_transfer
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end 
+
 end
+
+# binding.pry
